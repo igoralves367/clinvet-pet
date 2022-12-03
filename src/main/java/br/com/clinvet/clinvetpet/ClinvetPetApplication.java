6a6938ffdb88545845a983dcbabca0237bd63bdb
igoralves367 <igor.alves367@gmail.com>
2022-12-03 19:18:07 -0300

    Inica Projeto

---
+++ src/main/java/br/com/clinvet/clinvetpet/ClinvetPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinvet.clinvetpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinvetPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinvetPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clinvet/clinvetpet/ClinvetPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinvet.clinvetpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinvetPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
