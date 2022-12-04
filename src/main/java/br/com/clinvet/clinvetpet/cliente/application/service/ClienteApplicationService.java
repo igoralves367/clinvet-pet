4b49e1debd3d84fb49cf7dea0ad2ed110c3191c3
igoralves367 <igor.alves367@gmail.com>
2022-12-04 08:46:42 -0300

    Implementa fluxo Service

--- src/main/java/br/com/clinvet/clinvetpet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/clinvet/clinvetpet/cliente/application/service/ClienteApplicationService.java
@@ -1,11 +1,13 @@
 package br.com.clinvet.clinvetpet.cliente.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Service;
 
+import br.com.clinvet.clinvetpet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.clinvet.clinvetpet.cliente.application.api.ClienteListResponse;
 import br.com.clinvet.clinvetpet.cliente.application.api.ClienteRequest;
 import br.com.clinvet.clinvetpet.cliente.application.api.ClienteResponse;
@@ -35,5 +37,12 @@ public class ClienteApplicationService implements ClienteService {
 		List<Cliente> cliente = clienteRepository.buscaTodosClientes();
 		log.info("[finaliza] ClienteApplicationService - buscaTodosClintes");
 		return ClienteListResponse.converte(cliente);
+	}
+
+	@Override
+	public ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteApplicationService - buscaClienteAtravesId");
+		log.info("[finaliza] ClienteApplicationService - buscaClienteAtravesId");
+		return null;
 	}
 }
--- src/main/java/br/com/clinvet/clinvetpet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/clinvet/clinvetpet/cliente/application/service/ClienteService.java
@@ -1,7 +1,11 @@
 package br.com.clinvet.clinvetpet.cliente.application.service;
 
 import java.util.List;
+import java.util.UUID;
+
 import javax.validation.Valid;
+
+import br.com.clinvet.clinvetpet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.clinvet.clinvetpet.cliente.application.api.ClienteListResponse;
 import br.com.clinvet.clinvetpet.cliente.application.api.ClienteRequest;
 import br.com.clinvet.clinvetpet.cliente.application.api.ClienteResponse;
@@ -9,5 +13,6 @@ import br.com.clinvet.clinvetpet.cliente.application.api.ClienteResponse;
 public interface ClienteService {
 	ClienteResponse criaCliente(@Valid ClienteRequest clienteRequest);
 	List<ClienteListResponse> buscaTodosClintes();
+	ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente);
 
 }
