a8ae65ff0ecbce1070225f7c8961d860ef66f215
igoralves367 <igor.alves367@gmail.com>
2022-12-03 21:16:22 -0300

    Finaliza Metodo Busca todos os clientes

--- src/main/java/br/com/clinvet/clinvetpet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/clinvet/clinvetpet/cliente/infra/ClienteInfraRepository.java
@@ -25,8 +25,9 @@ public class ClienteInfraRepository implements ClienteRepository {
 	@Override
 	public List<Cliente> buscaTodosClientes() {
 		log.info("[inicia] ClienteInfraRepository - buscaTodosClientes");
+		List<Cliente> todosClientes = clienteSpringaDataJPARepository.findAll();
 		log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
-		return null;
+		return todosClientes;
 	}
 
 }
