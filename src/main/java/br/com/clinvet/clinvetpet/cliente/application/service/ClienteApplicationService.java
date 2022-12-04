c8c7ccdc93ad593a1e435e7b29c251f3f496d5cf
igoralves367 <igor.alves367@gmail.com>
2022-12-04 09:15:23 -0300

    Implementa fluxo repository e Finaliza Metodo Delete Cliente pelo Id

--- src/main/java/br/com/clinvet/clinvetpet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/clinvet/clinvetpet/cliente/application/service/ClienteApplicationService.java
@@ -50,6 +50,8 @@ public class ClienteApplicationService implements ClienteService {
 	@Override
 	public void deletaClienteAtravesId(UUID idCliente) {
 		log.info("[inicia] ClienteApplicationService - deletaClienteAtravesId");
+		Cliente cliente = clienteRepository.buscaClienteAtravesId(idCliente);
+		clienteRepository.deletaCliente(cliente);
 		log.info("[finaliza] ClienteApplicationService - deletaClienteAtravesId");
 		
 	}
